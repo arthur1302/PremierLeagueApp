93acdf7a44b4b72d5aa6fd6bf22ddb43a2424c9e
arthur_1302 <arthur.haus@student.hogent.be>
2023-12-26 11:59:17 +0100

    Eerste Team Card

--- app/src/main/java/com/example/premierleagueapp/MainActivity.kt
+++ app/src/main/java/com/example/premierleagueapp/MainActivity.kt
@@ -3,13 +3,28 @@ package com.example.premierleagueapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.premierleagueapp.ui.theme.PremierLeagueAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +35,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colorScheme.background,
                 ) {
                     Greeting("Android")
                 }
@@ -30,10 +45,47 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun Team() {
+    Box(modifier = Modifier.background(color = Color.White).padding(12.dp).border(1.dp, Color.Black).fillMaxWidth()) {
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Column(modifier = Modifier.padding(horizontal = 16.dp, vertical = 12.dp)) {
+                Text("Manchester", fontSize = 16.sp)
+                Text("England", fontSize = 12.sp)
+            }
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(50.dp)
+                    .background(shape = CircleShape, color = Color.White)
+                    .padding(end = 16.dp)
+                    .align(Alignment.CenterVertically),
+            )
+        }
+    }
+}
+
+@Composable
+fun TeamList() {
+    Column(modifier = Modifier.padding(10.dp).fillMaxWidth()) {
+        Team()
+        Team()
+    }
+}
+
+@Preview
+@Composable
+fun TeamPreview() {
+    PremierLeagueAppTheme {
+        TeamList()
+    }
+}
+
+@Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier,
     )
 }
 
